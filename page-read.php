2bc3b41359ff4b3bf86a8ede6426384f5246add6
Vladislav <v.kirbaba@gmail.com>
2016-02-08 16:12:54 +0300

    Читать главу

--- page-read.php
+++ page-read.php
@@ -9,102 +9,91 @@
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/@Elf@.jpg" alt="">
-					<span class="read__item--title">Глава 1</span>
+					<span class="read__item--title">Глава 1-2</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/chept2-1.png" alt="">
-					<span class="read__item--title">Глава 2</span>
+					<span class="read__item--title">Глава 3-4</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/chep3.png" alt="">
-					<span class="read__item--title">Глава 3</span>
+					<span class="read__item--title">Глава 5-6</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/chep3-1.png" alt="">
-					<span class="read__item--title">Глава 4</span>
+					<span class="read__item--title">Глава 7-8</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/img8844.png" alt="">
-					<span class="read__item--title">Глава 5</span>
+					<span class="read__item--title">Глава 9-10</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/img565412.png" alt="">
-					<span class="read__item--title">Глава 6</span>
+					<span class="read__item--title">Глава 11-12</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/chap7.png" alt="">
-					<span class="read__item--title">Глава 7</span>
+					<span class="read__item--title">Глава 13-14</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/chap8.png" alt="">
-					<span class="read__item--title">Глава 8</span>
+					<span class="read__item--title">Глава 15-16</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/chap9.png" alt="">
-					<span class="read__item--title">Глава 9</span>
+					<span class="read__item--title">Глава 17-18</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">
 					<img src="<?php bloginfo('template_directory'); ?>/img/chap10.png" alt="">
-					<span class="read__item--title">Глава 10</span>
+					<span class="read__item--title">Глава 19-20</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">					
-					<span class="read__item--title">Глава 11</span>
+					<span class="read__item--title">Глава 21-22</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">					
-					<span class="read__item--title">Глава 12</span>
+					<span class="read__item--title">Глава 23-24</span>
 				</span>
 			</a>
 			<a href="#" class="read__item">
 				<p class="book--title">Революция крови</p>
 				<span class="read__item--wrap">					
-					<span class="read__item--title">Глава 13</span>
+					<span class="read__item--title">Глава 25-26</span>
 				</span>
 			</a>
-			<a href="#" class="read__item">
-				<p class="book--title">Революция крови</p>
-				<span class="read__item--wrap">					
-					<span class="read__item--title">Глава 14</span>
-				</span>
-			</a>
-			<a href="#" class="read__item">
-				<p class="book--title">Революция крови</p>
-				<span class="read__item--wrap">					
-					<span class="read__item--title">Глава 15</span>
-				</span>
-			</a>
+			
 			<div class="preloader"></div>
 		</div>
 	</div>

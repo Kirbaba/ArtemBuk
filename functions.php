6969211a6ef6100868e3ad195247275db6d1c382
morozovigor <perffectgame@gmail.com>
2016-02-20 11:42:08 +0300

    modal more width

--- functions.php
+++ functions.php
@@ -92,6 +92,21 @@ function remove_admin_bar() {
     }
 }
 
+if (function_exists( 'add_theme_support' )){
+    add_filter('manage_posts_columns', 'posts_columns', 5);
+    add_action('manage_posts_custom_column', 'posts_custom_columns', 5, 2);
+    add_filter('manage_pages_columns', 'posts_columns', 5);
+    add_action('manage_pages_custom_column', 'posts_custom_columns', 5, 2);
+}
+function posts_columns($defaults){
+    $defaults['wps_post_thumbs'] = __('Thumbs');
+    return $defaults;
+}
+function posts_custom_columns($column_name, $id){
+    if($column_name === 'wps_post_thumbs'){
+        echo the_post_thumbnail( array(125,80) );
+    }
+}
 /*--------------------------------------------- МЕНЮ НАВИГАЦИИ -------------------------------------------------------*/
 
 function theme_register_nav_menu() {

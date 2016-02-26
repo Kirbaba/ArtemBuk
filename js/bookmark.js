08d6a3eb75338ac5c6394001fc73f028e88e0ba8
morozovigor <perffectgame@gmail.com>
2016-02-26 14:49:32 +0300

    bookmark to head

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,41 +1,50 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     setBookmark();
 
-    $('#readtext > p').on("click", function(){
+    $('#readtext > p').on("click", function () {
 
         var bookmark = $('#bookmark');
-        var post_link =  $('#readtext').attr('data-link');
-        var post_id =  $('#readtext').attr('data-post-id');
+        var post_link = $('#readtext').attr('data-link');
+        var post_id = $('#readtext').attr('data-post-id');
 
-        if(bookmark.length){
+        if (bookmark.length) {
             bookmark.remove();
         }
         $(this).before('<p id="bookmark"><i class="fa fa-bookmark"></i></p>');
 
         var index = $('#bookmark').index("p");
-        Cookies.set('bookmark',{link: post_link, pos: index, id: post_id},{expires: 7});
-        
+        Cookies.set('bookmark', {link: post_link, pos: index, id: post_id}, {expires: 7});
+
         setBookmarkLink();
     });
 });
 
-function setBookmarkLink(){
-    var cookie = jQuery.parseJSON(Cookies.get('bookmark'));
-   // console.log(cookie);
-    if(cookie.link){
-        $('.bookmarkBut').attr('href',cookie.link+'#bookmark');
+function setBookmarkLink() {
+    var cookieJson = Cookies.get('bookmark');
+
+    if (cookieJson) {
+        var cookie = jQuery.parseJSON(cookieJson);
+        // console.log(cookie);
+        if (cookie.link) {
+            $('.bookmarkBut').attr('href', cookie.link + '#bookmark');
+        }
     }
 }
 
-function setBookmark(){
-    var text =  $('#readtext');
-    if(text.length){
-        setBookmarkLink();
-        var cookie = jQuery.parseJSON(Cookies.get('bookmark'));
-        var currId = text.attr('data-post-id');
+function setBookmark() {
+    setBookmarkLink();
 
-        if(currId == cookie.id){
-            $("#readtext p").eq(cookie.pos).before('<p id="bookmark"><i class="fa fa-bookmark"></i></p>');
+    var text = $('#readtext');
+    if (text.length) {
+        var cookieJson = Cookies.get('bookmark');
+        if (cookieJson) {
+            var cookie = jQuery.parseJSON(cookieJson);
+            //   var cookie = jQuery.parseJSON(Cookies.get('bookmark'));
+            var currId = text.attr('data-post-id');
+
+            if (currId == cookie.id) {
+                $("#readtext p").eq(cookie.pos).before('<p id="bookmark"><i class="fa fa-bookmark"></i></p>');
+            }
         }
 
     }

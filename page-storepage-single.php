824a21e929834643f9fdc87c57516ee59fd51d95
Vladislav <v.kirbaba@gmail.com>
2016-03-10 15:39:30 +0300

    Купить книгу - сингл

---
+++ page-storepage-single.php
@@ -0,0 +1,78 @@
+<? get_header() ?>
+<div class="col-xs-12">
+	<div class="page__wrapper">
+	
+		<div class="page__head">
+			<h3>Купить книги 123/ </h3>
+		</div>
+		<div class="page__body page__body--wrap page__scrolltext storepage-single">			
+			<!-- <?php the_content(); ?> -->
+
+			<!-- open .storepage-single__head -->
+			<div class="storepage-single__head">
+				<!-- open .storepage-single__head--book -->
+				<div class="storepage-single__head--book csstransforms3d">
+				
+					<div class="book">
+						<span>
+							<img src="http://artembuk.com/wp-content/uploads/2016/02/2.jpg" alt="" />
+						</span>
+						
+					</div>
+				</div>
+				<!-- close .storepage-single__head--book -->
+				<!-- open .storepage-single__head--controls -->
+				<div class="storepage-single__head--controls">
+					<h3>Революция крови</h3>
+					<h4><i>Артем Бук</i></h4>
+					<a href="#" class="controls--read"><b><i>Читать книгу</i></b></a>
+					<!-- open .controls--format -->
+					<div class="controls--format">
+						<b><i>Скачать 50% книги</i></b>
+						<a href="#">pdf</a>
+						<a href="#">fb2</a>
+						<a href="#">epub</a>
+						<a href="#">rtf</a>
+						<a href="#">html</a>
+					</div>
+					<!-- close .controls--format -->
+					<a href="#" class="controls--buy"><b><i>Купить книгу<sup>за 97 рублей</sup></i></b></a>
+				</div>
+				<!-- close .storepage-single__head--controls -->
+
+			</div>
+			<!-- close .storepage-single__head -->
+
+			<!-- open .storepage-single--title -->
+			<h4 class="storepage-single--title">Аннотация</h4>                        
+			<!-- close .storepage-single--title -->
+			<article class="page__scrolltext storepage-single--annotation">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                <div class="preloader"></div>
+            </article>
+
+            <h4 class="storepage-single--title">Комментарии</h4> 
+            <article class="page__scrolltext storepage-single--annotation">
+	            <form action="/">
+	            	<textarea name="comment" id="comment" placeholder="Оставить комментарий (только для зарегистрированный пользователей)"></textarea>
+	            	<input type="submit" name="submit" class="comment-sub" value="Отправить"/>
+	            </form>
+				<!-- open .comment__item -->
+				<div class="comment__item">
+					<h4>Амадей88<span class="comment--time">4 марта 2016 17:48</span></h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+				</div>
+				<!-- close .comment__item -->
+				<!-- open .comment__item -->
+				<div class="comment__item">
+					<h4>Амадей88<span class="comment--time">4 марта 2016 17:48</span></h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+				</div>
+				<!-- close .comment__item -->
+            </article>
+
+			<div class="preloader"></div>
+		</div>
+	</div>
+</div>
+<?get_footer()?>

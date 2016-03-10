1d706daaeceaafcc77cd174887efdd34a274354c
Vladislav <v.kirbaba@gmail.com>
2016-03-10 12:06:41 +0300

    Купить  книгу

---
+++ page-storepage.php
@@ -0,0 +1,91 @@
+<? get_header() ?>
+
+	<div class="col-xs-4 col-xs-push-8">
+		<div class="welcome">
+			<div class="welcome__head">
+				<h4>Добро пожаловать на сайт писателя и путешественника Артема Бука!</h4>
+			</div>
+			<div class="welcome__box">
+				<p>Заинтересовала серия «Революция крови»? Просто нажмите <a href="/category/read/">«Читать книги».</a></p>
+				<p>В идиллическом мире будущего полицейский охотится на убийц своей подруги. 
+				Сможет ли он выжить в битве с могущественными тайными силами, и какую цену придется заплатить за месть? 
+				Выскажитесь о прочитанном в разделе <a href="/forum">«Форум»</a> и на страницах автора <a href="#">в соцсетях.</a></p>
+				<p>Любите ездить по миру или мечтаете об этом – загляните в раздел <a href="/category/trips/">«Путешествия»</a>.
+				Полезные советы по организации самостоятельной поездки.</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-8 col-xs-pull-4">
+		<div class="row">
+			<div class="page__wrapper">
+				<div class="page__head">
+					<h3>Купить книги</h3>
+				</div>
+				<div class="page__body page__body--wrap page__scrolltext read buyabook">
+					<!-- open .page-buyabook__row -->
+					<div class="buyabook__row">
+						<a href="#" class="buyabook__item">
+							<span class="buyabook__item--img">
+								<span>
+									<img src="http://artembuk.com/wp-content/uploads/2016/02/2.jpg" alt="" />
+								</span>
+								
+							</span>
+							<span class="buyabook__item--desc">
+								<p>Артем Бук</p>
+								<p>“Революция крови”</p>
+								<span class="buyabook__item--desc--price">250 руб</span>
+							</span>
+						</a>
+						<a href="#" class="buyabook__item">
+							<span class="buyabook__item--img">
+								<span>
+									<img src="http://artembuk.com/wp-content/uploads/2016/02/5.jpg" alt="" />
+								</span>
+								
+							</span>
+							<span class="buyabook__item--desc">
+								<p>Артем Бук</p>
+								<p>“Революция крови”</p>
+								<span class="buyabook__item--desc--price">250 руб</span>
+							</span>
+						</a>
+						<a href="#" class="buyabook__item">
+							<span class="buyabook__item--img">
+								<span>
+									<img src="http://artembuk.com/wp-content/uploads/2016/02/1.jpg" alt="" />
+								</span>								
+							</span>
+							<span class="buyabook__item--desc">
+								<p>Артем Бук</p>
+								<p>“Революция крови”</p>
+								<span class="buyabook__item--desc--price">250 руб</span>
+							</span>
+						</a>
+					</div>
+					<!-- close .page-buyabook__row -->
+					<!-- open .page-buyabook__row -->
+					<div class="buyabook__row">
+						<a href="#" class="buyabook__item">
+							<span class="buyabook__item--img--sub">
+								<img src="<?php bloginfo('template_directory'); ?>/img/adress-book.png" alt="" />
+								
+							</span>
+							<span class="buyabook__item--desc">
+								<p>Купить подписку</p>
+								<span class="buyabook__item--desc--price">10100 руб</span>
+							</span>
+						</a>
+					</div>
+					<!-- close .page-buyabook__row -->
+					<!-- open .page-buyabook__row -->
+					<div class="buyabook__row">
+						
+					</div>
+					<!-- close .page-buyabook__row -->
+					<div class="preloader"></div>
+				</div>
+			</div>		
+		</div>
+	</div>
+<?get_footer()?>

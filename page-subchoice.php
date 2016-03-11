36c235d5545df535464c34e456940e6a797b6b13
Vladislav <v.kirbaba@gmail.com>
2016-03-11 11:04:33 +0300

    Выбор подписки

---
+++ page-subchoice.php
@@ -0,0 +1,48 @@
+<? get_header() ?>
+	<div class="col-xs-12">
+		<div class="page__wrapper">
+			<div class="page__head">
+				<h4 class="align-center">Покупка подписки</h4>
+			</div>
+			<!-- open .page__scrolltext -->
+			<div class="page__scrolltext page-subscription">
+				<!-- open .subchoice__wrap -->
+				<div class="subchoice__wrap">
+					<!-- open .subchoice__item -->
+					<div class="subchoice__item">
+						<!-- open .subchoice__item--month -->
+						<div class="subchoice__item--month">
+							<!-- open .month--blue -->
+							<div class="month--blue">
+								<h2>6</h2>
+							</div>
+							<!-- close .month--blue -->
+							<h4>месяцев</h4>
+						</div>
+						<!-- close .subchoice__item--month -->
+						<a href="#" class="subchoice__item--btn">Купить подписку за <span class="yellow">300 руб</span></a>
+					</div>
+					<!-- close .subchoice__item -->
+					<!-- open .subchoice__item -->
+					<div class="subchoice__item">
+						<!-- open .subchoice__item--month -->
+						<div class="subchoice__item--month">
+							<!-- open .month--green -->
+							<div class="month--green">
+								<h2>12</h2>
+							</div>
+							<!-- close .month--green -->
+							<h4>месяцев</h4>
+						</div>
+						<!-- close .subchoice__item--month -->
+						<a href="#" class="subchoice__item--btn">Купить подписку за <span class="yellow">500 руб</span></a>
+					</div>
+					<!-- close .subchoice__item -->
+				</div>
+				<!-- close .subchoice__wrap -->
+				<div class="preloader"></div>
+			</div>
+			<!-- close .page__scrolltext -->
+		</div>
+	</div>
+<?get_footer()?>

c4e0171c717243232df5587ccfa4ae6301f67609
Vladislav <v.kirbaba@gmail.com>
2016-03-11 10:06:22 +0300

    Купить подписку

---
+++ page-subscription.php
@@ -0,0 +1,38 @@
+<? get_header() ?>
+	<!-- open .col-xs-8 -->
+	<div class="col-xs-5">
+		<!-- open .page__wrapper -->
+		<div class="page__wrapper">
+			<!-- open .page-subscription--left -->
+			<div class="page-subscription--left">
+				<img src="<?php bloginfo('template_directory'); ?>/img/adress-book-1.png" alt="" />
+				<a href="#">Купить подписку</a>
+			</div>
+			<!-- close .page-subscription--left -->
+		</div>
+		<!-- close .page__wrapper -->
+	</div>
+	<!-- close .col-xs-8 -->
+	<!-- open .col-xs-4 -->
+	<div class="col-xs-7">
+		<!-- open .page__wrapper -->
+		<div class="page__wrapper">
+			<div class="welcome__head page-subscription--head">
+				<h4>Концепция платной подписки</h4>
+			</div>
+			<!-- open .page__scrolltext -->
+			<div class="page__scrolltext page-subscription">
+				
+				<!-- open .ornament-top -->
+				<div class="ornament-top"></div>
+				<!-- close .ornament-top -->
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+				<div class="preloader"></div>
+			</div>
+			<!-- close .page__scrolltext -->
+		</div>
+		<!-- close .page__wrapper -->
+		
+	</div>
+	<!-- close .col-xs-4 -->
+<?get_footer()?>

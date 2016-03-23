aef66610bdb7cace627167ef2683c8c79b41b32a
morozovigor <perffectgame@gmail.com>
2016-03-23 12:43:05 +0300

    orders

--- category-read.php
+++ category-read.php
@@ -11,13 +11,23 @@
 			</div>
 			<div class="page__body page__body--wrap page__scrolltext">
 				<?php query_posts( array( 'posts_per_page' => -1, 'category_name' => 'read' ) ); ?>
+				<?php $i = 0; ?>
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php if($i==4){?>
+						<a href="/store/%D1%80%D0%B5%D0%B2%D0%BE%D0%BB%D1%8E%D1%86%D0%B8%D1%8F-%D0%BA%D1%80%D0%BE%D0%B2%D0%B8/" class="category__item">
+						<span class="category__item--wrap">
+							<img src="<?php bloginfo('template_directory'); ?>/img/adress-book-1.png" alt="">
+							<span class="read__item--title">Скачать книги</span>
+						</span>
+						</a>
+					<?php } ?>
 					<a href="<?php the_permalink(); ?>" class="category__item">
 						<span class="category__item--wrap">
 							<?php the_post_thumbnail(); ?>
 							<span class="read__item--title"><?php the_title(); ?></span>
 						</span>
 					</a>
+					<?php $i++; ?>
 				<?php endwhile; ?>
 				<?php  endif;?>
 				<!--<a href="#" class="category__item">

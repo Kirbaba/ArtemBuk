eef30a2e75b5f1821e94e6be9bd36a30e7b2cc43
Vladislav <v.kirbaba@gmail.com>
2016-03-23 13:21:06 +0300

    Кабинет старт

---
+++ page-cabinet.php
@@ -0,0 +1,36 @@
+<? get_header() ?>
+	<!-- open .col-xs-3 -->
+	<div class="col-xs-3">
+		<!-- open .page__wrapper -->
+		<div class="page__wrapper">
+			<!-- open .page__scrolltext -->
+			<div class="page__scrolltext">
+				<!-- open .welcome__head page-subscription--head -->
+				<div class="welcome__head page-subscription--head">
+					<h4>Личный кабинет</h4>
+				</div>
+				<!-- close .welcome__head page-subscription--head -->
+			<div class="preloader"></div>
+		</div>
+		<!-- close .page__scrolltext -->
+		</div>
+		<!-- close .page__wrapper -->
+		
+	</div>
+	<!-- close .col-xs-3 -->
+	<!-- open .col-xs-9 -->
+	<div class="col-xs-9">
+	<!-- open .page__wrapper -->
+	<div class="page__wrapper">
+		<!-- open .page__scrolltext -->
+		<div class="page__scrolltext">
+
+			<div class="preloader"></div>
+		</div>
+		<!-- close .page__scrolltext -->
+	</div>
+	<!-- close .page__wrapper -->
+		
+	</div>
+	<!-- close .col-xs-9 -->
+<?get_footer()?>

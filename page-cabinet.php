21d473b28b882bd5bd6b8704c85f7a3dd9ec5b6d
Vladislav <v.kirbaba@gmail.com>
2016-03-23 15:22:20 +0300

    Cabinet

--- page-cabinet.php
+++ page-cabinet.php
@@ -3,16 +3,30 @@
 	<div class="col-xs-3">
 		<!-- open .page__wrapper -->
 		<div class="page__wrapper">
+			<div class="welcome__head page-cabinet--head">
+				<h4>Личный кабинет</h4>
+			</div>
 			<!-- open .page__scrolltext -->
-			<div class="page__scrolltext">
-				<!-- open .welcome__head page-subscription--head -->
-				<div class="welcome__head page-subscription--head">
-					<h4>Личный кабинет</h4>
-				</div>
-				<!-- close .welcome__head page-subscription--head -->
-			<div class="preloader"></div>
-		</div>
-		<!-- close .page__scrolltext -->
+			<div class="page__scrolltext page-cabinet--aside">
+				 <!-- Nav tabs -->
+				<ul class="nav nav-tabs" role="tablist">
+					<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Профиль</a></li>
+					<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Подписки</a></li>
+					<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Заказы</a></li>
+					<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Корзина</a></li>
+					<li role="presentation"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Сообщения</a>
+						<ul class="dropdown-menu">
+					      	<li><a href="#">Входящие</a></li>
+		      				<li><a href="#">Отправленные</a></li>
+			      			<li><a href="#">Написать новое</a></li>	
+			      			<li><a href="#">Черновик</a></li>				      				
+					    </ul>
+					</li>
+				</ul>
+
+
+			</div>
+			<!-- close .page__scrolltext -->
 		</div>
 		<!-- close .page__wrapper -->
 		
@@ -23,9 +37,45 @@
 	<!-- open .page__wrapper -->
 	<div class="page__wrapper">
 		<!-- open .page__scrolltext -->
-		<div class="page__scrolltext">
+		<!-- open .page-cabinet__content--welcome -->
+		<div class="page-cabinet__content--welcome">
+			<p>Добро пожаловать, 
+				<span class="name">Roma887</span>
+				<span class="last">Последний вход: <i>Сегодня 10:36</i></span>
+			</p>
 
-			<div class="preloader"></div>
+		</div>
+		<!-- close .page-cabinet__content--welcome -->
+		<div class="page__scrolltext page-cabinet__content--wrap">
+			<div class="tab-content">
+			    <div role="tabpanel" class="tab-pane active" id="home">
+					<div class="page__head">
+						<h3>Профиль <a href="#" class="edit">Редактировать</a></h3>
+					</div>
+					<!-- open .page-cabinet__content--box -->
+					<div class="page-cabinet__content--box">
+						<!-- open .page-cabinet--avatar -->
+						<div class="page-cabinet--avatar">
+							
+						</div>
+						<!-- open .page-cabinet--information -->
+						<div class="page-cabinet--information">
+							<p>Проживание: <span>Москва</span></p>
+							<p>Пол: <span>Не указан</span></p>
+							<p>Дата рождения: <span>Не указана</span></p>
+							<p>Зарегистрирован: <span>21 марта 2016</span></p>
+							<p>Последний визит: <span>сегодня</span></p>
+							<p>Просмотр профиля: <span>0</span></p>
+						</div>
+						<!-- close .page-cabinet--information -->
+						<!-- close .page-cabinet--avatar -->
+					</div>
+					<!-- close .page-cabinet__content--box -->
+			    </div>
+			    <div role="tabpanel" class="tab-pane" id="profile">2</div>
+			    <div role="tabpanel" class="tab-pane" id="messages">3</div>
+			    <div role="tabpanel" class="tab-pane" id="settings">4</div>
+			</div>
 		</div>
 		<!-- close .page__scrolltext -->
 	</div>
